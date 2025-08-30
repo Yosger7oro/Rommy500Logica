168ddb7b22395aa4307c8afa42f1f4c49d372e71
Willian Alcon <1001.31258709.ucla@gmail.com>
2025-08-30 04:24:38 -0400

    cartas.py

--- cartas.py
+++ cartas.py
@@ -1,10 +1,13 @@
 class Cartas:
+    valores={"joker": 0,"a": 1, "j": 11, "q": 12, "k": 13}
+    mazo = 54
     def __init__(self, numero, figura):
         self.numero = numero
         self.figura = figura
 
-    #Se define una funcion cadena de texto, para la clase:
     def __str__(self):
         return f'{self.numero} de {self.figura}'
-    
-#soy tomas jajajajaj
+    def valor_numerico(self):
+        if self.numero.isdigit():
+            return int(self.numero)
+        return Cartas.valores[self.numero]
